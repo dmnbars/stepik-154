b86e9ad6674566b1beb75bdd3790e283c02423d2
dmnbars <dmnbars@gmail.com>
2020-04-13 23:45:21 +0300

    venv

--- init.sh
+++ init.sh
@@ -3,10 +3,10 @@
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 
-python3 -m venv venv
+virtualenv -p python3 venv
+source venv/bin/activate
 pip install --upgrade pip
 pip install django
 pip install gunicorn
 
-source venv/bin/activate
-gunicorn --bind=0.0.0.0:8080 --workers=2 --timeout=15 --log-level=debug hello.app
+gunicorn --bind=0.0.0.0:8080 --workers=2 --timeout=15 --log-level=debug hello:app
