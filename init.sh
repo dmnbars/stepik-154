9f3953e2cbff8e55494e1ade9096ebbe7339ff02
dmnbars <dmnbars@gmail.com>
2020-04-14 22:51:53 +0300

    lock django version

--- init.sh
+++ init.sh
@@ -6,7 +6,7 @@ sudo /etc/init.d/nginx restart
 virtualenv -p python3 venv
 source venv/bin/activate
 pip install --upgrade pip
-pip install django
+pip install django==2.0
 pip install gunicorn
 
 gunicorn --bind=0.0.0.0:8080 --workers=2 --timeout=15 --log-level=debug hello:app
