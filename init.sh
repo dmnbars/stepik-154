8728b885a8fdd0ff54654fb00d5446ae1596bc3e
dmnbars <dmnbars@gmail.com>
2020-04-14 23:51:37 +0300

    update init

--- init.sh
+++ init.sh
@@ -5,11 +5,11 @@ sudo /etc/init.d/nginx restart
 
 virtualenv -p python3 venv
 source venv/bin/activate
-pip install --upgrade pip
 pip install django==2.0
-pip install gunicorn
+pip install gunicorn==18
 
 cd /home/box/web/ask
 
 #gunicorn --bind=0.0.0.0:8080 --workers=2 --timeout=15 --log-level=debug hello:app
+#python manage.py runserver 0.0.0.0:8000
 gunicorn --bind=0.0.0.0:8000 --workers=2 --timeout=15 --log-level=debug ask.wsgi
